0a9e064256e79e97c16b77697cc4a88243eded68
Erfan Habibi <erfan.habibi6.79@gmail.com>
2024-01-14 14:15:45 +0330

    update js code and add overlay

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const navBtn = document.querySelector(".nav__btn")
 const navMenu = document.querySelector(".nav__menu-mobile__wrapper")
+const overlay = document.querySelector(".overlay")
 
 let navOpen = false;
 
@@ -7,10 +8,19 @@ navBtn.addEventListener("click", function() {
     if (navOpen) {
         navBtn.classList.remove("nav__btn--open")
         navMenu.classList.remove("nav__menu-mobile__wrapper--open")
+        overlay.classList.remove("overlay--show")
         navOpen = false
     } else {
         navBtn.classList.add("nav__btn--open")
         navMenu.classList.add("nav__menu-mobile__wrapper--open")
+        overlay.classList.add("overlay--show")
         navOpen = true
     }
+})
+
+overlay.addEventListener("click", function(){
+    navBtn.classList.remove("nav__btn--open")
+    navMenu.classList.remove("nav__menu-mobile__wrapper--open")
+    overlay.classList.remove("overlay--show")
+    navOpen = false
 })
